30bdcb55dede06fffa2d2a5e29c1c0a20eabb71c
Agustin Romero <agustin.romero.31.7.00@gmail.com>
2026-04-15 22:09:17 -0300

    Arreglé el código roto

--- Punto_2/codigo_sin_funcionar.c
+++ Punto_2/codigo_sin_funcionar.c
@@ -1,24 +1,28 @@
 // codigo_roto.c
+#include <stdio.h> //faltaba incluir la librería
 
-void duplicar_numero(int numero) {
-    numero = numero * 2;
+void duplicar_numero(int* numero)//recibe un puntero hacia el valor 
+{
+    *numero = *numero * 2;
 }
 
-int main() {
+int main() 
+{
     int valor1;
     int valor2;
+    int* p_valor1 = &valor1; //creo puntero hacia valor1
 
     printf("Ingrese el primer valor: ");
-    scanf("%d", valor1);
+    scanf("%d", &valor1); //este no guardaba el valor en la dirección de memoria
 
     printf("Ingrese el segundo valor: ");
     scanf("%d", &valor2);
 
-    int suma = valor1 + valor2
+    int suma = valor1 + valor2; //faltaba ; aquí
     printf("La suma es: %d\n", suma);
 
-    duplicar_numero(valor1);
+    duplicar_numero(p_valor1); //aquí mando el puntero hacia valor1 
     printf("El primer valor duplicado es: %d\n", valor1);
 
-    return 0
+    return 0;
 }

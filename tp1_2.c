8290f316646ffe5c644312502aa6ef4c182d9638
Agustin Romero <agustin.romero.31.7.00@gmail.com>
2026-04-28 21:19:22 -0300

    ejercicio 2.4 completado

--- tp1_2.c
+++ tp1_2.c
@@ -12,6 +12,25 @@ void devuelveCuadradoVoid (int* num)
     *num = (*num) * (*num);
 }
 
+void invertir (int* a, int* b)
+{
+    int a_temp;
+    a_temp = *a;
+    *a = *b;
+    *b = a_temp;
+}
+
+void orden (int* a, int* b)
+{
+    if (*a > *b)
+    {
+        int a_temp;
+        a_temp = *a;
+        *a = *b;
+        *b = a_temp;
+    }
+}
+
 int main ()
 {
     int num = 8;
@@ -22,6 +41,34 @@ int main ()
 
     printf("el contenido de la variable es: %d\n", *p_num);
     printf("la dirección de la variable es: %d\n", p_num);
+
+    int num1;
+    int num2;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num1);
+    printf("\n");
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num2);
+    printf("\n");
+
+    printf("el primer numero ingresado es: %d\n", num1);
+    printf("el segundo numero ingresado es: %d\n", num2);
+
+    int* p_num1;
+    int* p_num2;
+    p_num1 = &num1;
+    p_num2 = &num2;
+
+    invertir (p_num1, p_num2);
+
+    printf("el primer numero invertido es: %d\n", *p_num1);
+    printf("el segundo numero invertido es: %d\n", *p_num2);
+
+    orden (p_num1, p_num2);
+
+    printf("el numero menor es: %d\n", *p_num1);
+    printf("el segundo mayor es: %d\n", *p_num2);
 
     return 0;
 }
